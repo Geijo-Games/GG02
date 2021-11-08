914cad361acfe74789b82f36d23e02741a40c043
JiaChen ZENG <jiachen@zeng.games>
2021-11-08 15:53:52 +0800

    FEAT: リアクターを回転できるようにする。回転弾力は未実装

---
+++ Assets/Scenes/Experimental/RotateReactor/ReactorRotator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Rotate のアクションに応じてリアクターを回転させる。
+/// 回転弾力などの処理もここでする。
+/// </summary>
+public class ReactorRotator : MonoBehaviour
+{
+    private void Update()
+    {
+        // TODO: 回転弾力を加える
+    }
+
+    public void Rotate(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            // TODO: 差分を計算するため、初期回転角度をトラッキングする
+        }
+        else if (context.performed)
+        {
+            // TODO: 回転角度の差分によって回転
+            transform.localEulerAngles = new Vector3(0, 0, Vector2.SignedAngle(Vector2.right, context.ReadValue<Vector2>())); // TEMP
+        }
+        else if (context.canceled)
+        {
+            transform.localEulerAngles = Vector3.zero; // TEMP: 回転弾力を実装したら削除していい
+        }
+    }
+}
---
+++ Assets/Scenes/Experimental/RotateReactor/ReactorStick.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+
+/// <summary>
+/// リアクター用のスティックはゲームパッドをシミュレーションする。
+/// スティックの回転はリアクターと連動する。
+/// </summary>
+[AddComponentMenu("Input/On-Screen Stick")]
+public class ReactorStick : OnScreenControl, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    [SerializeField]
+    private GameObject Reactor;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData == null)
+            throw new System.ArgumentNullException(nameof(eventData));
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponentInParent<RectTransform>(), eventData.position, eventData.pressEventCamera, out Vector2 position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (eventData == null)
+            throw new System.ArgumentNullException(nameof(eventData));
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponentInParent<RectTransform>(), eventData.position, eventData.pressEventCamera, out var position);
+        var deltaPos = position - (Vector2)StartPos;
+
+        deltaPos = Vector2.ClampMagnitude(deltaPos, movementRange);
+        ((RectTransform)transform).anchoredPosition = StartPos + (Vector3)deltaPos;
+
+        var newPos = new Vector2(deltaPos.x / movementRange, deltaPos.y / movementRange);
+        SendValueToControl(newPos);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        ((RectTransform)transform).anchoredPosition = StartPos;
+        SendValueToControl(Vector2.zero);
+    }
+
+    private void Start()
+    {
+        StartPos = ((RectTransform)transform).anchoredPosition;
+    }
+
+    private void Update()
+    {
+        transform.localEulerAngles = Reactor.transform.localEulerAngles;
+        // TODO: 急に離したら、ゆっくりと元の位置に戻る
+    }
+
+    private float MovementRange = 10;
+
+    [field: SerializeField]
+    public float movementRange { get; set; } = 10;
+    private Vector3 StartPos;
+    [field: InputControl(layout = "Vector2")]
+    [field: SerializeField]
+    protected override string controlPathInternal { get; set; } = "<Gamepad>/leftStick";
+}
