8ce9462eef930ac7fa870caa87011885aca46bd8
monobe satoshi <sato1112mono@ezweb.ne.jp>
2021-11-17 00:26:10 +0900

    時間で動く蓋　左のみ

--- Assets/Scripts/LeftLidAnimation.cs
+++ Assets/Scripts/LeftLidAnimation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LidOpenAnimation : MonoBehaviour
+public class LeftLidAnimation : MonoBehaviour
 {
     Animator animator;
     int LidNum = 0;
@@ -10,12 +10,10 @@ public class LidOpenAnimation : MonoBehaviour
     bool isLeftLidOpening = false;
     bool isLeftLidClosingWarning = false;
     bool isLeftLidClosing = false;
-
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-
     }
 
     // Update is called once per frame
@@ -23,13 +21,25 @@ public class LidOpenAnimation : MonoBehaviour
     {
         StartCoroutine("LidAnimation");
     }
+
     IEnumerator LidAnimation()
     {
         LidNum = 1;
         animator.SetInteger("LidNumber", 1);
         Debug.Log("Yaik!");
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(3.0f);
         Debug.Log("Yaik!");
         animator.SetBool("LeftLidOpenWarning", true);
+        yield return new WaitForSeconds(3.0f);
+        animator.SetBool("LeftLidOpenWarning", false);
+        animator.SetBool("LeftLidOpening", true);
+        yield return new WaitForSeconds(5.0f);
+        animator.SetBool("LeftLidOpening", false);
+        animator.SetBool("LeftLidCloseWarning", true);
+        yield return new WaitForSeconds(3.0f);
+        animator.SetBool("LeftLidCloseWarning", false);
+        animator.SetBool("LeftLidClosing", true);
     }
 }
+
+
