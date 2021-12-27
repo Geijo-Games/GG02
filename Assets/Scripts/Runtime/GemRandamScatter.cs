9cfa60d86c991455712d8eec642d9bf14237f389
monobe satoshi <monobe.satoshi.017@s.kyushu-u.ac.jp>
2021-12-27 22:28:48 +0900

    宝石のランダム生成ができない

--- Assets/Scripts/Runtime/GemRandamScatter.cs
+++ Assets/Scripts/Runtime/GemRandamScatter.cs
@@ -9,7 +9,7 @@ public class GemRandamScatter : MonoBehaviour
     [SerializeField] public BoxCollider2D _blockBoxCollider;
 
     private const float _MIN_INTERVAL_VALUE = 0.5f;
-    private const float _MAX_INTERVAL_VALUE = 2.0f;
+    private const float _MAX_INTERVAL_VALUE = 1.0f;
     private const float _MIN_X_VALUE = -1.0f;
     private const float _MAX_X_VALUE = 1.0f;
     private const float _MIN_Y_VALUE = 0.5f;
@@ -27,6 +27,7 @@ public class GemRandamScatter : MonoBehaviour
     private float _randomValueY;
     public GameObject prefabObj;
     Vector3 pos = Vector3.zero;
+    float GemCount;
 
     private readonly List<Vector3> _usePositionList = new List<Vector3>();
 
@@ -34,16 +35,16 @@ public class GemRandamScatter : MonoBehaviour
     void Start()
     {
         //空同然だけどリスト作っとく
-        foreach (Transform child in _blockParent.transform)
+        foreach (Vector3 child in _blockParent.transform)
         {
-            _usePositionList.Add(child.position);
+            _usePositionList.Add(child);
         }
 
-        DelayInitBlock();
-        foreach (Transform child in _blockParent.transform)
+        RandomPos();
+        foreach (Vector3 child in _usePositionList)
         {
             Vector3 pos = Vector3.zero;
-            child.position += pos;
+            pos += child;
             Instantiate(prefabObj,pos , Quaternion.identity);
         }
 
@@ -54,24 +55,12 @@ public class GemRandamScatter : MonoBehaviour
     {
         
     }
-    void  DelayInitBlock()
+    void RandomPos()
     {
-        while (_isGameStart)
+        for(GemCount=0; GemCount < 11; GemCount++)
         {
-            //ランダムな値
-            _randomNumber = Random.Range(0, _blockParent.transform.childCount);
-            _randomInterval = Random.Range(_MIN_INTERVAL_VALUE, _MAX_INTERVAL_VALUE);
             _randomValueX = Random.Range(_MIN_X_VALUE, _MAX_X_VALUE);
             _randomValueY = Random.Range(_MIN_Y_VALUE, _MAX_Y_VALUE);
-           
-
-            //選ばれたブロックの位置
-            Vector3 selectedBlockPosition =
-                    _blockParent.transform.GetChild(_randomNumber).gameObject.transform.position;
-
-            //選ばれたブロックの位置は比較対象から一旦削除
-            _usePositionList.Remove(selectedBlockPosition);
-
             //現在使用中のポジションのリストから今利用検討中のポジションが利用可能か判定
             foreach (Vector3 position in _usePositionList)
             {
@@ -81,25 +70,19 @@ public class GemRandamScatter : MonoBehaviour
                 _isSetablePositionY =
                     Mathf.Abs(position.y - _randomValueY) > _blockBoxCollider.bounds.size.y;
 
-
-                //座標のうち、全ての軸で被っていたら置けないのでやり直し
-                if (!_isSetablePositionX && !_isSetablePositionY)
-                {
-                    _usePositionList.Add(selectedBlockPosition);
-                    break;
-                }
             }
 
             //位置被りがどれか1つの軸で無ければ実行する
-            if (_isSetablePositionX || _isSetablePositionY )
+            if (_isSetablePositionX || _isSetablePositionY)
             {
                 Vector3 randomPosition = new Vector3(_randomValueX, _randomValueY);
 
-               
+
 
                 //新しい使用中のポジションをリストに追加
                 _usePositionList.Add(randomPosition);
             }
         }
     }
+    
 }
