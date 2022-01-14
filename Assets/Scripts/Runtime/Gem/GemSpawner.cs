da06b88f94778b2e336954859d72abc63f76e0a0
LIU RUIXIAO <misakanyan1@yahoo.co.jp>
2022-01-14 18:11:49 +0900

    FEAT: #25 宝石を複数生成できるようにする

--- Assets/Scripts/Runtime/Gem/GemSpawner.cs
+++ Assets/Scripts/Runtime/Gem/GemSpawner.cs
@@ -8,6 +8,8 @@ public class GemSpawner : MonoBehaviour
     public Vector3 SpawnPosOffset;
     public float SpawnSpeed;
     public float SpawnInterval;
+    public int SpawnNum;
+    public float SpawnEachGemOffset;
 
     private float m_SpawnTimer;
 
@@ -24,10 +26,19 @@ public class GemSpawner : MonoBehaviour
         m_SpawnTimer += Time.deltaTime;
         if(m_SpawnTimer > SpawnInterval)
         {
-            GameObject obj = GemPool.Instance.Spawn("Gem", transform.position + SpawnPosOffset, transform.rotation);
-            if (obj && obj.TryGetComponent(out Rigidbody2D rigidbody))
+            for(int i = 0; i < SpawnNum; i++)
             {
-                rigidbody.AddForce(new Vector2(transform.up.x, transform.up.z) * SpawnSpeed);
+                Vector3 pos = Vector3.zero;
+                float _randomValueX = Random.Range(-0.5f, 0.5f);
+                float _randomValueY = Random.Range(-0.5f, 0.5f);
+                pos.x = _randomValueX + transform.position.x;
+                pos.y = _randomValueY + transform.position.y;
+                GameObject obj = GemPool.Instance.Spawn("Gem", pos, transform.rotation);
+                if (obj && obj.TryGetComponent(out Rigidbody2D rigidbody))
+                {
+                    float _SpawnSpeed = SpawnSpeed * Random.Range(0.9f, 1.1f);
+                    rigidbody.AddForce(new Vector2(transform.up.x, transform.up.z) * SpawnSpeed);
+                }
             }
             m_SpawnTimer = 0;
         }
